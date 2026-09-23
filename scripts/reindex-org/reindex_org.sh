23d53feb13ac337d3db493c992337af2707c2c9b
kentsanggds <ken.tsang@digital.cabinet-office.gov.uk>
2026-09-23 17:49:47 +0100

    Set ARG env var unless it is passed into the shell script
    Running this on a container seemed to ignore any args passed to the script so set ARG env var before running the script in the container

--- scripts/reindex-org/reindex_org.sh
+++ scripts/reindex-org/reindex_org.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-echo "Updating $1"
+if [ -z "${ARG}" ]; then
+  ARG="$1"
+fi
+
+echo "Updating $ARG"
 curl -g "$CKAN_SOLR_URL/update?commit=true" \
      -H 'Content-Type: application/json' \
-     -d '{"delete":{"query":"site_id:dgu_organisations_2 AND name:'$1'"}}'
+     -d '{"delete":{"query":"site_id:dgu_organisations_2 AND name:'$ARG'"}}'
 
 ckan datagovuk reindex-organisations
